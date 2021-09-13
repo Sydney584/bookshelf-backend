0191d28c81f7d3102f5c395f97bd40080a7925ee
sydney <sydneychase5841@gmail.com>
2021-09-13 03:18:45 -0400

    updated bookshelf controller, added def show

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -1,12 +1,19 @@
 class BookshelvesController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
     # GET /bookshelves
     def index
       bookshelves = Bookshelf.all
       render json: bookshelves 
     end
+
+    # POST /bookshelves
+  def create
+    bookshelf = Bookshelf.create!(bookshelf_params)
+      render json: bookshelf, status: :created
+  end
 
     #GET /bookshelves/:id
     def show
