81d95832dcf2de290ef21d6c753e7dacd23ab533
sydney <sydneychase5841@gmail.com>
2021-09-13 01:00:15 -0400

    updated cors file, updated gem file, added show, error message, private methods

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -1,7 +1,26 @@
 class BookshelvesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     # GET /bookshelves
     def index
       bookshelves = Bookshelf.all
       render json: bookshelves 
     end
+
+    #GET /bookshelves/:id
+    def show
+        bookshelf = find_bookshelf
+        render json: bookshelf
+      
+      end
+
+      private
+
+      def render_not_found_response
+        render json: { error: "Bookshelf not found" }, status: :not_found
+      end
+
+      def find_bookshelf
+        Bookshelf.find(params[:id])
+      end
 end
